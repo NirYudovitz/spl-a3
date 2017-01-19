2505a3b4b9a061977393b81c714e99f9c5565f84
nirdun <nirdun@gmail.com>
2017-01-19 17:53:35 +0200

    main server

--- src/main/java/mainServer.java
+++ src/main/java/mainServer.java
@@ -8,24 +8,23 @@ import bgu.spl171.net.srv.Server;
 public class mainServer {
     public static void main(String[] args) {
 
-        if(args.length==0||args.length>1){
-            System.out.println("Usage: <port>");
-            System.exit(1);
-        }
-        String s=args[0];
-        int port=-1;
-        try{
-            port=Integer.parseInt(s);
-        }catch (NumberFormatException e){
-            System.out.println("Usage: <port> only");
-            System.exit(1);
-        }
-        if(port<1||port>65535){
-            System.out.println("Usage: <port>  and port should be in range 1 through 65535");
-            System.exit(1);
-        }
-        System.out.println("Thread Per Client server has being started on port:"+port);
-
+//        if(args.length==0||args.length>1){
+//            System.out.println("Usage: <port>");
+//            System.exit(1);
+//        }
+//        String s=args[0];
+//        int port=-1;
+//        try{
+//            port=Integer.parseInt(s);
+//        }catch (NumberFormatException e){
+//            System.out.println("Usage: <port> only");
+//            System.exit(1);
+//        }
+//        if(port<1||port>65535){
+//            System.out.println("Usage: <port>  and port should be in range 1 through 65535");
+//            System.exit(1);
+//        }
+//        System.out.println("Thread Per Client server has being started on port:"+port);
         Server.threadPerClient(
                 7777, //port
                 () -> new BaseProtocol(), //protocol factory
